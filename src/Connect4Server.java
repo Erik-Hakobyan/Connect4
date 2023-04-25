dcbf795af7e2e7d1628a9d97c315f6be8bc803f0
Danyal Ahmad <da2853@nyu.edu>
2023-04-25 18:38:30 -0400

    Update 3

--- src/Connect4Server.java
+++ src/Connect4Server.java
@@ -1,18 +1,60 @@
 import javax.swing.*;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
 
-public class Connect4Server {
+public class Connect4Server extends Thread{
+    private int port;
+    private boolean live;
+    Hashtable<Integer, ClientConnection> Games = new Hashtable<>();
+
+    public Connect4Server(int port_config){
+        port = port_config;
+        live = true;
+        run();
+    }
+
+    public void stopServer(){
+        live = false;
+    }
+
+    @Override
+    public void run(){
+        try {
+            System.out.println("Server IP Address: "  + InetAddress.getLocalHost().getHostAddress());
+            System.out.println("Port: " + Integer.toString(port));
+            ServerSocket sock = new ServerSocket(port);
+            while (live){
+                Socket socket = sock.accept();
+                ClientConnection con = new ClientConnection();
+
+            }
 
 
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+class ClientConnection{
+        public ClientConnection(){
+            run();
+        }
 
 
-
+}
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
-
-                new Connect4Server();
+                int port = 1234;
+                new Connect4Server(port);
             }
         });
 
