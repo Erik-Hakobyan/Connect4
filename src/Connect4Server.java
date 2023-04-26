fb11cfe4a7adb9468f8f5949e70a7660fc58fc6a
Danyal Ahmad <da2853@nyu.edu>
2023-04-26 00:28:42 -0400

    Update 4
    Signed-off-by: Danyal Ahmad <da2853@nyu.edu>

--- src/Connect4Server.java
+++ src/Connect4Server.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.SecureRandom;
 import java.util.Hashtable;
 
 public class Connect4Server extends Thread{
@@ -28,7 +32,7 @@ public class Connect4Server extends Thread{
             ServerSocket sock = new ServerSocket(port);
             while (live){
                 Socket socket = sock.accept();
-                ClientConnection con = new ClientConnection();
+                ClientConnection con = new ClientConnection(socket);
             }
 
         } catch (UnknownHostException e) {
@@ -39,12 +43,25 @@ public class Connect4Server extends Thread{
 
     }
 
-    class ClientConnection {
-        public ClientConnection() {
-            start();
+    class ClientConnection extends Thread {
+        private static final String SYMBOLS = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        private BufferedReader in;
+        private PrintStream out;
+        private int game_mode;
+        private String name, username, game_id;
+        Socket sock;
+
+        public ClientConnection(Socket socket) {
+            sock = socket;
+            if (verifyConnection()) {
+                initializeMode();
+            }
         }
 
         private void newGame() {
+            String gameKey = generateKey();
+
+
         }
 
         private void joinGame() {
@@ -53,16 +70,52 @@ public class Connect4Server extends Thread{
         private void spectate() {
         }
 
-    }
+        private static String generateKey() {
+            SecureRandom random = new SecureRandom();
+            StringBuilder sb = new StringBuilder(8);
 
-    class DatabaseManager {
+            for (int i = 0; i < 8; i++) {
+                int randomIndex = random.nextInt(SYMBOLS.length());
+                char randomChar = SYMBOLS.charAt(randomIndex);
+                sb.append(randomChar);
+            }
+            return sb.toString();
 
-    }
+        }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+        private void initializeMode() {
+
+        }
+
+
+        private boolean verifyConnection() {
+            try {
+                in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                out = new PrintStream(sock.getOutputStream());
+                String line = in.readLine();
+                String[] parts = line.split(",");
+                if (parts[0] == "Authenticate") {
+                    game_mode = Integer.parseInt(parts[1]);
+                    name = parts[2];
+                    username = parts[3];
+                    game_id = parts[4];
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+
+        class DatabaseManager {
+
+        }
+
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
                 int port = 1234;
                 new Connect4Server(port);
             }
