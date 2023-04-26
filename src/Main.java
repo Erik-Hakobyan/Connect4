9c286c9cb227e2e7188dfe8d84a552e8d2bd6091
Danyal Ahmad <da2853@nyu.edu>
2023-04-26 01:07:18 -0400

    Update 4
    Signed-off-by: Danyal Ahmad <da2853@nyu.edu>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
         int port = 1234;
         new Connect4Server(port);
         new Connect4User();
